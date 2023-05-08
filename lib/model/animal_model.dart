94974084f9198532b8fa5d0cab593b0b1605503a
Jasitharan Pakeerathan <jasitharan1998@gmail.com>
2023-05-08 20:38:24 +0530

    commit

---
+++ lib/model/animal_model.dart
@@ -0,0 +1,13 @@
+class Animal {
+  String imageUrl;
+  String title;
+  String subtitle;
+  String fact;
+
+  Animal({
+    required this.imageUrl,
+    required this.title,
+    required this.subtitle,
+    required this.fact,
+  });
+}
